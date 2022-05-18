ea2ae223f511df650587865416522e6d0632f89b
Mario Ruiz <marioarb.97@gmail.com>
2022-05-17 23:36:07 -0500

    Initial commit: proyecto vacío

---
+++ src/main/java/com/pickanis/api/PickanisApiApplication.java
@@ -0,0 +1,13 @@
+package com.pickanis.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickanisApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickanisApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pickanis/api/PickanisApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pickanis.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickanisApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
